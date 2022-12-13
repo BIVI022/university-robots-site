e6bdb11bf68d24d52462d60b7e88232c2eab2a22
danik yurkevich <dy@iamit.net>
2022-12-13 21:54:46 +0300

    Add nav items handlers -scrolling to sections

--- common/layouts/Main/components/Navigation.tsx
+++ common/layouts/Main/components/Navigation.tsx
@@ -1,14 +1,64 @@
+import mainLayoutStyles from '../MainLayout.module.scss';
+import mainViewStyles from '../../../../views/Main/Main.module.scss';
+
 interface NavigationProps {
     className?: string;
 }
 
 const Navigation = ({ className = '' }: NavigationProps) => {
+    const scrollToContacts = () => {
+        const appWrapperNode = document.querySelector<HTMLDivElement>(
+            `.${mainLayoutStyles.wrapper}`
+        );
+        if (appWrapperNode) {
+            window.scrollTo({
+                top: appWrapperNode.offsetHeight,
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    const scrollToWelcomeSection = () => {
+        const welcomeSection = document.querySelector<HTMLDivElement>(
+            `.${mainViewStyles.welcome}`
+        );
+        if (welcomeSection) {
+            window.scrollTo({
+                top: welcomeSection.offsetTop,
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    const scrollToAboutSection = () => {
+        const aboutSection = document.querySelector<HTMLDivElement>(
+            `.${mainViewStyles.about}`
+        );
+        if (aboutSection) {
+            window.scrollTo({
+                top: aboutSection.offsetTop,
+                behavior: 'smooth',
+            });
+        }
+    };
+    const scrollToCoursesSection = () => {
+        const coursesSection = document.querySelector<HTMLDivElement>(
+            `.${mainViewStyles.courses}`
+        );
+        if (coursesSection) {
+            window.scrollTo({
+                top: coursesSection.offsetTop,
+                behavior: 'smooth',
+            });
+        }
+    };
+
     return (
         <nav className={className}>
-            <div>Главная</div>
-            <div>О нас</div>
-            <div>Контакты</div>
-            <div>Курсы</div>
+            <div onClick={scrollToWelcomeSection}>Главная</div>
+            <div onClick={scrollToAboutSection}>О нас</div>
+            <div onClick={scrollToContacts}>Контакты</div>
+            <div onClick={scrollToCoursesSection}>Курсы</div>
         </nav>
     );
 };
